faf923e3a7c97d249b440b069cdaeca8843ffc13
santhosh <santhoshaa2023@gmail.com>
2026-07-21 11:59:40 +0530

    upi update

--- backend/config.py
+++ backend/config.py
@@ -2,8 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "custom_wear_cradance_secret_key_12345")
-    TOKEN_EXPIRATION_HOURS = 24
+    TOKEN_EXPIRATION_HOURS = 87600
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static", "uploads")
     
     # Ensure upload folders exist
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+ 
